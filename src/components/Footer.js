4005762c0100c0a91f667a8031fcc4a647230814
Kevin Zumbado <kzumbado23@gmail.com>
2023-02-04 16:24:38 -0600

    Co-authored-by: WilmerDev20 <WilmerDev20@users.noreply.github.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -44,7 +44,7 @@ function Footer() {
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink className='text-orange-200 uppercase text-sm md:text-base opacity-70 hover:opacity-100'>
+                    <NavLink to={'/contact'} className='text-orange-200 uppercase text-sm md:text-base opacity-70 hover:opacity-100'>
                         Contacto
                     </NavLink>
                 </li>
