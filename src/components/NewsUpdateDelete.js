0181474692ca1059986a81733d0936bcf6d13188
kzumbado <61952168+kzumbado@users.noreply.github.com>
2023-03-21 17:17:47 -0700

    Revert Changes

--- src/components/NewsUpdateDelete.js
+++ src/components/NewsUpdateDelete.js
@@ -4,7 +4,7 @@ import TextareaAutosize from 'react-textarea-autosize';
 import { useForm } from '../hooks/useForm';
 import { MdDelete,MdSave} from 'react-icons/md'
 import { useDispatch } from 'react-redux';
-import {startDeleteNews, startLoadingNews, startUpdateNews} from '.././store/news/thunks'
+import {startDeleteNews, startUpdateNews} from '.././store/news/thunks'
 
 const NewsUpdateDelete = ({news={}}) => {
 
@@ -13,7 +13,7 @@ const NewsUpdateDelete = ({news={}}) => {
     description:news.description
    });
 
-   const [image, setImage]= useState([]);
+   const [image, setImage]= useState([])
 
    const dispatch= useDispatch();
 
@@ -32,10 +32,10 @@ const NewsUpdateDelete = ({news={}}) => {
         event.preventDefault();
         const id=news.id;
         const date=news.date;
-        const imageURL=news.image;
+        const imageURL=news.imageURL;
         const imageID=news.imageID;
-        const file= image;
-        
+        const file=image;
+       
         const n={
             title,
             description,
@@ -46,57 +46,47 @@ const NewsUpdateDelete = ({news={}}) => {
             file
         }
         dispatch(startUpdateNews(n));
-        dispatch(startLoadingNews());
   }
 
 
   const onDelete=()=>{
     dispatch(startDeleteNews(news.id,news.imageID));
-   
 }
-
 
   return (
     <div className='w-full h-full'>
  
-            <form onSubmit={onSubmit} className='flex flex-col w-full h-auto mx-2 items-center '>
-                <input onChange={onChangeForm} type='text' placeholder='Title' value={title} name='title' className='w-full pl-2  my-2'/>
+      <form onSubmit={onSubmit} className='flex flex-col w-full h-auto mx-2 items-center '>
+          <input onChange={onChangeForm} type='text' placeholder='Title' value={title} name='title' className='w-full pl-2  my-2'/>
 
-                <TextareaAutosize onChange={onChangeForm} placeholder='Description' value={description} name='description' maxRows={3} className='w-full pl-2 my-2  resize-none'/>
+          <TextareaAutosize onChange={onChangeForm} placeholder='Description' value={description} name='description' maxRows={3} className='w-full pl-2 my-2  resize-none'/>
 
-                <input 
-                  type="file" 
-                  ref={fileInputRef}
-                  onChange={onFileInputChange}
-                  style={{display:'none'}}
-                />
+          <input 
+            type="file" 
+            ref={fileInputRef}
+            onChange={onFileInputChange}
+            style={{display:'none'}}
+          />
 
-                <button
-                    type='button'
-                    onClick={()=>fileInputRef.current.click()}
-                    title='Upload Images'
-                    className='text-5xl mr-2 my-2 text-primary-200'>
-                    <img src={news.image} alt={news.title} className='w-[200px] h-[200px]'></img>
-                </button>
+          <button
+              type='button'
+              onClick={()=>fileInputRef.current.click()}
+              title='Upload Images'
+              className='text-5xl mr-2 my-2 text-primary-200'>
+              <img src={news.imageURL} alt={news.title} className='w-[200px] h-[200px]'></img>
+          </button>
 
-                <div className='flex flex-row justify-between w-[200px]'>
-                    <button title='Update'>
-                        <MdSave className='w-[30px] h-[30px] '/>
-                    </button>
+          <div className='flex flex-row justify-between w-[200px]'>
+              <button title='Update'>
+                  <MdSave className='w-[30px] h-[30px] '/>
+              </button>
 
-                    <button onClick={onDelete} title='Delete' type='button'>
-                        <MdDelete className='w-[30px] h-[30px] '/>
-                    </button>
-                </div>
+              <button onClick={onDelete} title='Delete' type='button'>
+                  <MdDelete className='w-[30px] h-[30px] '/>
+              </button>
+          </div>
 
-            </form>
-
-
-
-
-
-
-
+      </form>
     </div>
   )
 }
